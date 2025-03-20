2cab7e57f5f7b9fd00c6a054b4cc5dc07d4885b5
xXWarfaaXx <121723272+xXWarfaaXx@users.noreply.github.com>
2025-03-20 10:18:34 +0000

    Create SignUp.php
    This handles the user signup on the webpage that I am creating
    Have

---
+++ SignUp.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include "db.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+    $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $email, $password);
+
+    if ($stmt->execute()) {
+        echo "Signup successful! <a href='login.php'>Login here</a>";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+}
+?>
+
+<form method="post">
+    <input type="text" name="username" placeholder="Username" required><br>
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Sign Up</button>
+</form>
