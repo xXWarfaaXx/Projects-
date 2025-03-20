55f01805801683e4e0c96490a38cd454d360cfaa
xXWarfaaXx <121723272+xXWarfaaXx@users.noreply.github.com>
2025-03-20 10:21:20 +0000

    Create dashboard.php
    A simple protected page that requires login.

---
+++ dashboard.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION["user_id"])) {
+    header("Location: login.php");
+    exit();
+}
+
+echo "Welcome, " . $_SESSION["username"] . "!";
+echo "<br><a href='logout.php'>Logout</a>";
+?>
